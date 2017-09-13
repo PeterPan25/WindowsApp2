d79e36e16e2b2cc70d21cd1f1d14d4af7f60f136
Henner <H.majchrzak@gmx.de>
2017-09-13 18:28:10 +0200

    test

--- WindowsApp2/Form1.Designer.vb
+++ WindowsApp2/Form1.Designer.vb
@@ -278,6 +278,7 @@ Partial Class Form1
         Me.LinkLabel28 = New System.Windows.Forms.LinkLabel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.CB_name = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.SC_Aaliyah, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SC_Aaliyah.Panel1.SuspendLayout()
         Me.SC_Aaliyah.Panel2.SuspendLayout()
@@ -3409,11 +3410,21 @@ Partial Class Form1
         Me.CB_name.Size = New System.Drawing.Size(262, 21)
         Me.CB_name.TabIndex = 8
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(314, 58)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1010, 603)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.CB_name)
         Me.Controls.Add(Me.SC_Aaliyah)
         Me.Controls.Add(Me.SC_Fenya)
@@ -3796,4 +3807,5 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents CB_name As ComboBox
+    Friend WithEvents Button1 As Button
 End Class
