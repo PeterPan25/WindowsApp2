fb2d20d01412433f9d806f063fec29c80a6a0020
Henner <H.majchrzak@gmx.de>
2017-10-12 23:51:14 +0200

    Platzhalter

--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -29,28 +29,6 @@
 
 
 
-    ' Private Sub B_öffnen_Click(sender As Object, e As EventArgs) Handles B_öffnen.Click
-
-    ' Funktioniert für Textbox
-    'Dim name As String
-    'name = "F_" + TextBox1.Text
-
-    'If name = My.Forms.F_Aaliyah.Name Then
-    '    F_Aaliyah.Show()
-    'ElseIf name = My.Forms.F_Peter.Name Then
-    '    F_Peter.Show()
-    'ElseIf name = My.Forms.F_Hans.Name Then
-    '    F_Hans.Show()
-
-    'Else
-
-
-    '    MessageBox.Show("Kind nicht vorhanden")
-
-    'End If
-
-    '   End Sub
-
 
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
@@ -217,8 +195,7 @@
         CB_name.DataSource = name3
     End Sub
 
+    Private Sub Schule1_Load(sender As Object, e As EventArgs) Handles Schule1.Load
 
-
-
-
+    End Sub
 End Class
