bc914f14287f05d6b0398d6d1bde775a7927c4d9
FLX01 <don_fl@gmx.de>
2017-10-24 16:42:11 +0200

    loginform+begrüssungsdisplay

--- WindowsApp2/Ansichten/UserControl1.Designer.vb
+++ WindowsApp2/Ansichten/UserControl1.Designer.vb
@@ -22,8 +22,41 @@ Partial Class UserControl1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(38, 69)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(322, 218)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'UserControl1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "UserControl1"
+        Me.Size = New System.Drawing.Size(874, 532)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- WindowsApp2/Ansichten/UserControl1.vb
+++ WindowsApp2/Ansichten/UserControl1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class UserControl1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        LoginForm1.Show()
+    End Sub
 End Class
