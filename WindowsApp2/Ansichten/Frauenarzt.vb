54ed894db1a43023679a7ca550556df456fa54b2
FLX01 <don_fl@gmx.de>
2017-11-23 15:47:02 +0100

    Frauenarzt anlegen

--- WindowsApp2/Ansichten/Frauenarzt.vb
+++ WindowsApp2/Ansichten/Frauenarzt.vb
@@ -1,4 +1,34 @@
 ﻿Public Class Frauenarzt
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As DataRow = Me.DataSet1.Frauenarzt.NewRow()
+
+        Try
+            a("F_Name") = PlatzhalterText1.Text
+            a("Plz") = PlatzhalterText2.Text
+            a("Ort") = PlatzhalterText3.Text
+            a("Strasse") = PlatzhalterText4.Text
+
+            Me.DataSet1.Frauenarzt.AddFrauenarztRow(a)
+            Me.FrauenarztTableAdapter.Update(Me.DataSet1)
+            Me.DatenLaden()
+
+        Catch ex As Exception
+            MsgBox("Schade")
+        End Try
+
+
+    End Sub
+
+
+    Private Sub DatenLaden()
+        ' Me.FrauenarztTableAdapter.Fill(DataSet1.Frauenarzt)
+        Me.FrauenarztDataGridView.Update()
+
+    End Sub
+
+
     'Private Sub FrauenarztBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles MyBaseBindingNavigatorSaveItem.Click
     '    Me.Validate()
     '    Me.FrauenarztBindingSource.EndEdit()
