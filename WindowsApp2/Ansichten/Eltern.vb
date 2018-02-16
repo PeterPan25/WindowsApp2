b964ec0f288511054283b28d6a322a2a582084c3
Henner <H.majchrzak@gmx.de>
2018-02-16 16:09:37 +0100

    icons

--- WindowsApp2/Ansichten/Eltern.vb
+++ WindowsApp2/Ansichten/Eltern.vb
@@ -31,6 +31,10 @@
         Me.ElternBerichtDataGridView.Update()
     End Sub
 
+    Private Sub SplitContainer1_Panel1_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel1.Paint
+
+    End Sub
+
 
     'Private Sub ElternBerichtBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles MyBaseBerichtBindingNavigatorSaveItem.Click, MyBaseBerichtBindingNavigatorSaveItem.Click
     '    Me.Validate()
